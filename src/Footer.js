32a342cc7545a21ca4a7ad620c30a32b74753d57
John Poppington <johnpoppington@Johns-MacBook-Pro.local>
2022-10-04 22:20:54 -0400

    React app for deployment

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = ({ length }) => {
+    return (
+        <footer>
+            <p>
+                {length} List {length === 1 ? "Item" : "Items"}
+            </p>
+        </footer>
+  )
+}
+
+export default Footer
